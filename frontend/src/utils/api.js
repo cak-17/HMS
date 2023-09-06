bd14f65a99cd2624f5ba9fa0bc3b9e24ae957e39
cak-17 <98436543+cak-17@users.noreply.github.com>
2023-09-06 02:57:29 +0200

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,5 +1,48 @@
-import axios from 'axios'
+import axios from 'axios';
 
-export default axios.create({
-    baseUrl: "http://0.0.0.0:8000/api/"
-})
+const apiInstance = axios.create({
+  baseURL: 'http://localhost:3000/api', 
+});
+
+class ApiRepository {
+  constructor(endpoint) {
+    this.endpoint = endpoint;
+  }
+
+  async getAll() {
+    try {
+      const response = await apiInstance.get(`/${this.endpoint}`);
+      return response.data;
+    } catch (error) {
+      this.handleApiError(error);
+    }
+  }
+
+  async create(newData) {
+    try {
+      const response = await apiInstance.post(`/${this.endpoint}`, newData);
+      return response.data;
+    } catch (error) {
+      this.handleApiError(error);
+    }
+  }
+  
+  async getAll() {
+    try {
+      const response = await apiInstance.get(`/${this.endpoint}`);
+      return response.data;
+    } catch (error) {
+      this.handleApiError(error);
+    }
+  }
+
+
+  // Other CRUD methods...
+
+  handleApiError(error) {
+    console.error(`API Error in ${this.endpoint}:`, error);
+    throw error;
+  }
+}
+
+export default ApiRepository;
